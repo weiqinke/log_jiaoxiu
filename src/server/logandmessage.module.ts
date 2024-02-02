f0e8d492ca1d6e91f4c31e614f00d9aff4495a93
weiqinke <928148936@qq.com>
2024-02-02 12:06:14 +0800

    feat: 更新配置sql

--- src/server/logandmessage.module.ts
+++ src/server/logandmessage.module.ts
@@ -5,6 +5,7 @@ import { ProfileEntity } from '@/entitys/profile.entity';
 import { UserEntity } from '@/entitys/user.entity';
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LogController } from './log/log.controller';
 import { LogService } from './log/log.service';
@@ -15,13 +16,21 @@ import { PlacardService } from './placard/placard.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      // 自定义.env文件路径（默认为根路径的 .env 文件），如果有多个，可以传递数组
+      envFilePath: '.mysql.env',
+      // 控制是否不解析.env文件，只使用运行时注入的环境变量
+      ignoreEnvFile: false,
+      // 是否是全局模块
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'test',
-      password: '12345678',
-      database: 'test',
+      host: process.env.DATABASE_host,
+      port: parseInt(process.env.DATABASE_database),
+      username: process.env.DATABASE_username,
+      password: process.env.DATABASE_password,
+      database: process.env.DATABASE_database,
       entities: [AccontlogEntity, NoticeEntity, PlacardEntity, UserEntity, ProfileEntity],
       synchronize: true,
     }),
